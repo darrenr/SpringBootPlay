01db5022570da1030b963276d7417f2254397997
Darren Rowley <rowleydm@gmail.com>
2015-06-20 15:44:39 +1200

    basic hello world working

---
+++ src/main/java/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    
+    protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+}
